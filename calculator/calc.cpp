43bb6fd9ff23fe1f156e63bf2428c6375bad43e4
Popitka994 <144613923+Popitka994@users.noreply.github.com>
2023-12-02 23:22:25 +0300

    Update calc.cpp

--- calculator/calc.cpp
+++ calculator/calc.cpp
@@ -3,67 +3,65 @@
 #include <algorithm>
 #include <numeric>
 #include <iomanip>
+using namespace std;
 
-void printUsage()
+void Instruction()
 {
-    std::cout << "Usage: calculator [--operation operation] number1 number2 ... numberN\n";
-    std::cout << "Options:\n";
-    std::cout << "  --operation operation: Specify the operation to perform (sr_arif or mediana)\n";
+    cout << "Использование: ./calc [--operation operation] number1 number2 ... numberN.\n";
+    cout << "Операция подсчета среднего арифметического: --operation sr_arif. Операция медианы: --operation mediana.\n";
+    cout << "Количество операндов может варьироваться от 5 до 7.\n";
 }
 
-double calculateMean(const std::vector<double>& numbers)
+double sr_arif(const vector<double>& numbers)
 {
-    double sum = std::accumulate(numbers.begin(), numbers.end(), 0.0);
+    double sum = accumulate(numbers.begin(), numbers.end(), 0.0);
     return sum / numbers.size();
 }
 
-double calculateMedian(std::vector<double>& numbers)
+double median(vector<double>& numbers)
 {
-    std::sort(numbers.begin(), numbers.end());
+    sort(numbers.begin(), numbers.end());
     size_t size = numbers.size();
-    if (size % 2 == 0) {
-        return (numbers[size / 2 - 1] + numbers[size / 2]) / 2.0;
-    } else {
-        return numbers[size / 2];
-    }
+    if (size % 2 == 0) return (numbers[size / 2 - 1] + numbers[size / 2]) / 2.0;
+    else return numbers[size / 2];
 }
 
 int main(int argc, char* argv[])
 {
     if (argc < 2) {
-        printUsage();
+        Instruction();
         return 0;
     }
 
-    std::string operation;
-    std::vector<double> numbers;
+    string operation;
+    vector<double> numbers;
     
     for (int i = 1; i < argc; ++i) {
-        std::string arg = argv[i];
-        if (arg == "--operation") { // Определение операции
+        string arg = argv[i];
+        if (arg == "--operation") {
             if (i + 1 < argc) { 
                 operation = argv[i + 1];
                 ++i;
             }
-        } else { // Заполнение вектора операндами
-            double number = std::stod(arg);
+        } else {
+            double number = stod(arg);
             numbers.push_back(number);
         }
     }
 
     if (numbers.size() < 5 || numbers.size() > 7) {
-        std::cout << "Error: Number of operands should be between 5 and 7.\n";
+        cout << "Ошибка: Количество операндов может варьироваться от 5 до 7!\n";
         return 0;
     }
     
     if (operation == "sr_arif") {
-        double mean = calculateMean(numbers);
-        std::cout << "Mean: " << std::fixed << std::setprecision(2) << mean << "\n";
+        double mean = sr_arif(numbers);
+        cout << "Среднее арифметическое = " << fixed << setprecision(2) << mean << "\n";
     } else if (operation == "mediana") {
-        double median = calculateMedian(numbers);
-        std::cout << "Median: " << std::fixed << std::setprecision(2) << median << "\n";
+        double median = median(numbers);
+        cout << "Медиана = " << fixed << setprecision(2) << median << "\n";
     } else {
-        std::cout << "Error: Unknown operation.\n";
+        cout << "Ошибка: неизвестная операция!.\n";
         return 0;
     }
     
